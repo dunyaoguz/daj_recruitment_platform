c788b8b00263598b3da0aa55d34d01002c593833
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2021-08-10 00:43:27 -0400

    create triggers, insert statements and change schema

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,6 +1,44 @@
+DROP DATABASE IF EXISTS recruitment_platform;
 CREATE DATABASE recruitment_platform;
+USE recruitment_platform;
 
-CREATE TABLE membership_types (
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_registered DATETIME DEFAULT NOW(),
+  user_type ENUM('Employer', 'Recruiter', 'Job Seeker', 'Administrator') NOT NULL,
+  login_name VARCHAR(100) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  phone VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(256) NOT NULL UNIQUE,
+  status ENUM('Active', 'Deactivated') DEFAULT 'Active'
+);
+CREATE TRIGGER users_insert_trigger AFTER INSERT
+  ON users FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value)
+    VALUES ('users', 'new user was created', JSON_OBJECT("user_type", NEW.user_type,
+                                                         "login_name", NEW.login_name,
+                                                         "email", NEW.email));
+    -- creation of a user automatically creates an account
+    INSERT INTO accounts(user_id) VALUES (NEW.id);
+    END;
+CREATE TRIGGER users_update_trigger BEFORE UPDATE
+  ON users FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value, old_value)
+    VALUES ('users', 'user information was updated', JSON_OBJECT("user_type", NEW.user_type,
+                                                                 "login_name", NEW.login_name,
+                                                                 "phone", NEW.phone,
+                                                                 "email", NEW.email,
+                                                                 "status", NEW.status),
+                                                     JSON_OBJECT("user_type", OLD.user_type,
+                                                                 "login_name", OLD.login_name,
+                                                                 "phone", OLD.phone,
+                                                                 "email", OLD.email,
+                                                                 "status", OLD.status));
+    END;
+
+CREATE TABLE memberships (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_type ENUM('Employer', 'Job Seeker') NOT NULL,
   membership_type ENUM('Basic', 'Prime', 'Gold') NOT NULL,
@@ -11,10 +49,42 @@ CREATE TABLE membership_types (
 
 CREATE TABLE accounts (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  transaction_date DATETIME DEFAULT NOW(),
-  amount FLOAT(7, 5) NOT NULL,
-  balance FLOAT(7, 5) NOT NULL
+  user_id INT NOT NULL,
+  balance FLOAT(7, 2) DEFAULT 0,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
+CREATE TRIGGER accounts_update_trigger BEFORE UPDATE
+  ON accounts FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value, old_value)
+    VALUES ('accounts', 'account balance was updated', JSON_OBJECT("balance", NEW.balance), JSON_OBJECT("balance", OLD.balance));
+    END;
+
+CREATE TABLE transactions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  account_id INT NOT NULL,
+  transaction_date DATETIME DEFAULT NOW(),
+  transaction_type ENUM('Payment', 'Charge') NOT NULL,
+  -- negative if its a charge, positive if its a payment
+  amount FLOAT(7, 2) NOT NULL,
+  FOREIGN KEY (account_id)
+    REFERENCES accounts(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+CREATE TRIGGER transactions_insert_trigger AFTER INSERT
+  ON transactions FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value)
+    VALUES ('transactions', 'new transaction was recorded', JSON_OBJECT("transaction_date", NEW.transaction_date,
+                                                                        "transaction_type", NEW.transaction_type,
+                                                                        "amount", NEW.amount));
+    -- Following trigger updates the balance attribute in the accounts table
+    UPDATE accounts SET balance = balance + NEW.amount WHERE id = NEW.account_id;
+    END;
 
 CREATE TABLE payment_methods (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,9 +92,9 @@ CREATE TABLE payment_methods (
   payment_method_type ENUM('Credit', 'Debit') NOT NULL,
   billing_address VARCHAR(256) NOT NULL,
   postal_code VARCHAR(20) NOT NULL,
-  card_number INT NOT NULL CONSTRAINT CHECK (LENGTH(card_number) BETWEEN 15 AND 16),
-  security_code TINYINT NOT NULL CONSTRAINT CHECK (LENGTH(security_code)=3),
-  expiration_month TINYINT NOT NULL CONSTRAINT CHECK (LENGTH(expiration_month)=2),
+  card_number VARCHAR(20) NOT NULL UNIQUE CONSTRAINT CHECK (LENGTH(card_number) BETWEEN 15 AND 16),
+  security_code SMALLINT(3) ZEROFILL NOT NULL,
+  expiration_month TINYINT(2) ZEROFILL NOT NULL,
   expiration_year SMALLINT NOT NULL CONSTRAINT CHECK (LENGTH(expiration_year)=4),
   withdrawal_method ENUM('Manual', 'Automatic') DEFAULT 'Manual',
   is_active BOOLEAN DEFAULT TRUE,
@@ -33,36 +103,59 @@ CREATE TABLE payment_methods (
       ON UPDATE CASCADE
       ON DELETE CASCADE
 );
+CREATE TRIGGER payment_methods_insert_trigger AFTER INSERT
+  ON payment_methods FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value)
+    VALUES ('payment_methods', 'new payment method was added', JSON_OBJECT("account_id", NEW.account_id,
+                                                                           "payment_method_type", NEW.payment_method_type,
+                                                                           "withdrawal_method", NEW.withdrawal_method));
+    END;
+CREATE TRIGGER payment_methods_update_trigger BEFORE UPDATE
+  ON payment_methods FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value, old_value)
+    VALUES ('payment_methods', 'payment method was updated', JSON_OBJECT("account_id", NEW.account_id,
+                                                                         "payment_method_type", NEW.payment_method_type,
+                                                                         "billing_address", NEW.billing_address,
+                                                                         "postal_code", NEW.postal_code,
+                                                                         "withdrawal_method", NEW.withdrawal_method,
+                                                                         "is_active", NEW.is_active),
+                                                            JSON_OBJECT("account_id", OLD.account_id,
+                                                                        "payment_method_type", OLD.payment_method_type,
+                                                                        "billing_address", OLD.billing_address,
+                                                                        "postal_code", OLD.postal_code,
+                                                                        "withdrawal_method", OLD.withdrawal_method,
+                                                                        "is_active", OLD.is_active));
+    END;
 
 CREATE TABLE employers (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  membership_type_id INT NOT NULL,
-  account_id INT NOT NULL,
+  user_id INT NOT NULL,
+  membership_id INT,
   name VARCHAR(50) NOT NULL,
-  date_registered DATETIME DEFAULT NOW(),
-  login_name VARCHAR(100) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  FOREIGN KEY (membership_type_id)
-    REFERENCES membership_types(id)
-      ON UPDATE CASCADE,
-  FOREIGN KEY (account_id)
-    REFERENCES accounts(id)
+  FOREIGN KEY (membership_id)
+    REFERENCES memberships(id)
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
       ON UPDATE CASCADE
       ON DELETE CASCADE
 );
 
 CREATE TABLE recruiters (
   id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
   employer_id INT NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  date_registered DATETIME DEFAULT NOW(),
-  login_name VARCHAR(100) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  phone_number VARCHAR(50) NOT NULL,
-  email VARCHAR(256) NOT NULL,
   FOREIGN KEY (employer_id)
     REFERENCES employers(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
       ON UPDATE CASCADE
       ON DELETE CASCADE
 );
@@ -74,9 +167,8 @@ CREATE TABLE jobs (
   date_posted DATETIME DEFAULT NOW(),
   title VARCHAR(100) NOT NULL,
   description VARCHAR(256) NOT NULL,
-  required_experience INT NOT NULL,
+  required_experience INT,
   status ENUM('Open', 'Closed') DEFAULT 'Open',
-  category -- TO COMPLETE
   city VARCHAR(50) NOT NULL,
   province VARCHAR(50) NOT NULL,
   country VARCHAR(50) NOT NULL,
@@ -89,29 +181,58 @@ CREATE TABLE jobs (
     REFERENCES recruiters(id)
       ON UPDATE CASCADE
 );
+CREATE TRIGGER jobs_insert_trigger AFTER INSERT
+  ON jobs FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value)
+    VALUES ('jobs', 'new job was posted', JSON_OBJECT("title", NEW.title,
+                                                      "description", NEW.description,
+                                                      "city", NEW.city,
+                                                      "province", NEW.province,
+                                                      "country", NEW.country,
+                                                      "is_remote_eligible", NEW.is_remote_eligible));
+    END;
+CREATE TRIGGER jobs_update_trigger BEFORE UPDATE
+  ON jobs FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value, old_value)
+    VALUES ('jobs', 'job was updated', JSON_OBJECT("title", NEW.title,
+                                                   "description", NEW.description,
+                                                   "is_remote_eligible", NEW.is_remote_eligible,
+                                                   "status", NEW.status),
+                                       JSON_OBJECT("title", OLD.title,
+                                                   "description", OLD.description,
+                                                   "is_remote_eligible", OLD.is_remote_eligible,
+                                                   "status", OLD.status));
+    END;
+
+CREATE TABLE job_categories (
+  job_id INT NOT NULL,
+  job_category VARCHAR(256) NOT NULL,
+  FOREIGN KEY (job_id)
+    REFERENCES jobs(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  PRIMARY KEY (job_id, job_category)
+);
 
 CREATE TABLE job_seekers (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  membership_type_id INT NOT NULL,
-  account_id INT NOT NULL,
+  user_id INT NOT NULL,
+  membership_id INT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  date_registered DATETIME DEFAULT NOW(),
-  login_name VARCHAR(100) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  current_title VARCHAR(100) NOT NULL,
-  years_of_experience INT NOT NULL,
-  phone_number VARCHAR(50) NOT NULL,
-  email VARCHAR(256) NOT NULL,
+  current_title VARCHAR(100),
+  years_of_experience INT,
   city VARCHAR(50) NOT NULL,
   province VARCHAR(50) NOT NULL,
   country VARCHAR(50) NOT NULL,
-  status ENUM('Active', 'Deleted') DEFAULT 'Active',
-  FOREIGN KEY (membership_type_id)
-    REFERENCES membership_types(id)
-      ON UPDATE CASCADE,
-  FOREIGN KEY (account_id)
-    REFERENCES accounts(id)
+  FOREIGN KEY (membership_id)
+    REFERENCES memberships(id)
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
       ON UPDATE CASCADE
       ON DELETE CASCADE
 );
@@ -120,6 +241,7 @@ CREATE TABLE job_seeker_education_history (
   job_seeker_id INT NOT NULL,
   education_type ENUM('High School', 'Bachelors', 'Masters', 'PhD', 'Certificate', 'Diploma') NOT NULL,
   school VARCHAR(100) NOT NULL,
+  concentration VARCHAR(100) NOT NULL,
   grade FLOAT(3, 2),
   year_graduated SMALLINT NOT NULL CONSTRAINT CHECK (LENGTH(year_graduated)=4),
   FOREIGN KEY (job_seeker_id)
@@ -132,18 +254,48 @@ CREATE TABLE job_seeker_education_history (
 CREATE TABLE applications (
   id INT AUTO_INCREMENT PRIMARY KEY,
   job_seeker_id INT NOT NULL,
+  job_id INT NOT NULL,
   date_applied DATETIME DEFAULT NOW(),
   status ENUM('In Progress', 'Submitted', 'Withdrawn', 'Rejected', 'Offered', 'Accepted') NOT NULL,
   FOREIGN KEY (job_seeker_id)
     REFERENCES job_seekers(id)
       ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  FOREIGN KEY (job_id)
+    REFERENCES jobs(id)
+      ON UPDATE CASCADE
       ON DELETE CASCADE
 );
+CREATE TRIGGER applications_insert_trigger AFTER INSERT
+  ON applications FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value)
+    VALUES ('applications', 'new application was made', JSON_OBJECT("job_seeker_id", NEW.job_seeker_id,
+                                                                    "job_id", NEW.job_id));
+    END;
+CREATE TRIGGER applications_update_trigger BEFORE UPDATE
+  ON applications FOR EACH ROW
+    BEGIN
+    INSERT INTO system_logs (table_name, activity, new_value, old_value)
+    VALUES ('applications', 'application status changed', JSON_OBJECT("job_seeker_id", NEW.job_seeker_id,
+                                                                      "job_id", NEW.job_id,
+                                                                      "status", NEW.status),
+                                                          JSON_OBJECT("job_seeker_id", OLD.job_seeker_id,
+                                                                      "job_id", OLD.job_id,
+                                                                      "status", OLD.status));
+    END;
 
 CREATE TABLE administrators (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  login_name VARCHAR(100) NOT NULL,
-  password VARCHAR(256) NOT NULL
+  last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE system_logs (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  activity_date DATETIME DEFAULT NOW(),
+  table_name VARCHAR(50) NOT NULL,
+  activity VARCHAR(50) NOT NULL,
+  old_value JSON,
+  new_value JSON
 );
--- database/insert_statements.sql
+++ database/insert_statements.sql
@@ -1,6 +1,84 @@
-INSERT INTO membership_types (user_type, membership_type, monthly_fee, job_posting_limit, job_application_limit) VALUES
-    ('Employer', 'Prime', 50, 5, 0),
-    ('Employer', 'Gold', 100, NULL, 0),
-    ('Job Seeker', 'Basic', 0, 0, 0),
-    ('Job Seeker', 'Prime', 10, 0, 5),
-    ('Job Seeker', 'Gold', 20, 0, NULL);
+INSERT INTO users (user_type, login_name, password, phone, email) VALUES
+  ('Employer', 'spotify', 'spotify123456', '4385016383', 'spotify@spotify.com'),
+  ('Employer', 'renorun', 'renorun123456', '5145016383', 'renorun@renorun.com'),
+  ('Employer', 'busbud', 'busbud123456', '4385026383', 'busbud@busbud.com'),
+  ('Recruiter', 'ally_giordino', 'ally123456', '4385016393', 'ally@spotify.com'),
+  ('Recruiter', 'karen_banahora', 'karen123456', '4585016383', 'karen@renorun.com'),
+  ('Recruiter', 'emily_tetzal', 'emily123456', '4375016383', 'emily@busbud.com'),
+  ('Job Seeker', 'dunya_oguz', 'dunyas_password', '6385016383', 'dunya@concordia.ca'),
+  ('Job Seeker', 'azman_akhter', 'azmans_password', '4385096383', 'azman@concordia.ca'),
+  ('Job Seeker', 'john_purcell', 'johns_password', '4385015383', 'john@concordia.ca');
+
+INSERT INTO memberships (user_type, membership_type, monthly_fee, job_posting_limit, job_application_limit) VALUES
+  ('Employer', 'Prime', 50, 5, 0),
+  ('Employer', 'Gold', 100, NULL, 0),
+  ('Job Seeker', 'Basic', 0, 0, 0),
+  ('Job Seeker', 'Prime', 10, 0, 5),
+  ('Job Seeker', 'Gold', 20, 0, NULL);
+
+UPDATE users SET status = 'Deactivated' where login_name = 'john_purcell';
+
+INSERT INTO transactions (account_id, transaction_type, amount) VALUES
+  (1, 'Charge', -100),
+  (2, 'Charge', -50),
+  (3, 'Charge', -50),
+  (7, 'Charge', -20),
+  (8, 'Charge', -10),
+  (9, 'Charge', -10);
+
+INSERT INTO transactions (account_id, transaction_type, amount) VALUES
+  (1, 'Payment', 200),
+  (2, 'Payment', 10),
+  (3, 'Payment', 30),
+  (4, 'Payment', 27),
+  (5, 'Payment', 13),
+  (6, 'Payment', 8);
+
+INSERT INTO payment_methods (account_id, payment_method_type, billing_address, postal_code,
+                              card_number, security_code, expiration_month, expiration_year) VALUES
+  (1, 'Credit', '60 Rue de Bresoles', 'H2Y 1V5', '001836352043266', 677, 09, 2023),
+  (2, 'Debit', '1467 Saint Denis Street', 'H2X 3J5', '356799032150000', 098, 12, 2025),
+  (3, 'Credit', '2066 Jeanne Mance St', 'H2X 2J5', '9871234099654399', 123, 01, 2022),
+  (4, 'Debit', '87 Atwater Avenue', 'H3H 1B5', '997659811304114', 591, 04, 2030),
+  (5, 'Credit', '1212 Avenue Des Pins', 'H2W 1S6', '766665098121243', 432, 10, 2022),
+  (6, 'Debit', '3665 Rue de Bullion', 'H2X 3A6', '8888076542187610', 404, 07, 2023);
+
+UPDATE payment_methods SET postal_code = 'H2Y 8PO' WHERE account_id = 5;
+
+INSERT INTO employers (user_id, membership_id, name) VALUES
+  (1, 2, 'Spotify'),
+  (2, 1, 'Renorun'),
+  (3, 1, 'Busbud');
+
+INSERT INTO recruiters (user_id, employer_id, first_name, last_name) VALUES
+  (4, 1, 'Ally', 'Giordino'),
+  (5, 2, 'Karen', 'Banahora'),
+  (6, 3, 'Emily', 'Tetzal');
+
+INSERT INTO jobs (employer_id, recruiter_id, title, description, city, province, country) VALUES
+  (1, 1, 'Software Engineer', 'This is the best job ever.', 'Montreal', 'QC', 'Canada'),
+  (2, 2, 'Data Scientist', 'This job is fun.', 'Toronto', 'ON', 'Canada'),
+  (3, 3, 'Engineering Manager', 'This job rocks!', 'New York City', 'New York', 'USA');
+
+INSERT INTO job_seekers (user_id, membership_id, first_name, last_name, city, province, country) VALUES
+  (7, 3, 'Dunya', 'Oguz', 'Montreal', 'QC', 'Canada'),
+  (8, 4, 'Azman', 'Ahkter', 'Toronto', 'ON', 'Canada'),
+  (9, 5, 'John', 'Purcell', 'Hallifax', 'Nova Scotia', 'Canada');
+
+INSERT INTO job_seeker_education_history (job_seeker_id, education_type, school, concentration, grade, year_graduated) VALUES
+  (1, 'Diploma', 'Concordia University', 'Computer Science', 3.8, 2022),
+  (2, 'Diploma', 'Concordia University', 'Computer Science', 3.5, 2021),
+  (3, 'Diploma', 'Concordia University', 'Computer Science', 3.1, 2021);
+
+INSERT INTO applications (job_seeker_id, job_id) VALUES
+  (1, 1),
+  (2, 3),
+  (3, 2);
+
+INSERT INTO administrators (first_name, last_name) VALUES
+  ('Dunya', 'Oguz'),
+  ('Azman', 'Akhter'),
+  ('John', 'Purcell');
+
+UPDATE applications SET status = 'Accepted' WHERE job_seeker_id = 1 AND job_id = 1;
+UPDATE jobs SET status = 'Closed' WHERE id = 1;
