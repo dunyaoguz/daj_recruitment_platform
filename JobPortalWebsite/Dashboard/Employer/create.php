add8bbcfad1572b48fbccc9478c85872532e89eb
Azman Akhter <azman.akhter@gmail.com>
2021-08-13 18:15:49 -0400

    BugFix

--- JobPortalWebsite/Dashboard/Employer/create.php
+++ JobPortalWebsite/Dashboard/Employer/create.php
@@ -4,13 +4,13 @@ include_once('../../database.php');
 //$user_id = $_SESSION['userId'];
 $user_id = "1";
 
-if(isset($_POST["job_title"]) && isset($_POST["job_description"]) && isset($_POST["job_experienceRequired"]) && isset($_POST["job_city"]) && isset($_POST["job_province"]) && isset($_POST["job_country"])){
+if(isset($_POST["job_title"]) && isset($_POST["job_description"]) && isset($_POST["job_experience"]) && isset($_POST["job_city"]) && isset($_POST["job_province"]) && isset($_POST["job_country"])){
     $job = $conn->prepare("INSERT INTO jobs (employer_id, recruiter_id, title, description, required_experience, city, province, country)
                             VALUES (:employer_id, :recruiter_id, :title, :description, :required_experience, :city, :province, :country)");
     
     $job->bindParam(':title', $_POST["job_title"]);
     $job->bindParam(':description', $_POST["job_description"]);
-    $job->bindParam(':required_experience', $_POST["job_experienceRequired"]);
+    $job->bindParam(':required_experience', $_POST["job_experience"]);
     $job->bindParam(':city', $_POST["job_city"]);
     $job->bindParam(':province', $_POST["job_province"]);
     $job->bindParam(':country', $_POST["job_country"]);
@@ -52,13 +52,13 @@ if(isset($_POST["job_title"]) && isset($_POST["job_description"]) && isset($_POS
        <form action="./create.php" method = "post">
     
         <label for="job_title">Job Title</label><br>
-        <input type="text" name = "job_title" id = "book_title"><br>
+        <input type="text" name = "job_title" id = "job_title"><br>
 
         <label for="job_description">Job Description</label><br>
         <input type="text" name = "job_description" id = "job_description"><br>
 
-        <label for="job_experienceRequired">Job Experience Required (Years)</label><br>
-        <input type="number" name = "job_experienceRequired" id = "job_experienceRequired"><br>
+        <label for="job_experience">Job Experience Required (Years)</label><br>
+        <input type="number" name = "job_experience" id = "job_experience"><br>
 
         <label for="job_city">City</label><br>
         <input type="text" name = "job_city" id = "job_city"><br>
