fad14bad4c750aa152a5092a1a73d90906f5aeff
John Purcell <j_purc@grace.encs.concordia.ca>
2021-08-13 19:31:06 -0400

    Almost finished Employer Signup

--- JobPortalWebsite/SignUp/Employer/Company/index.php
+++ JobPortalWebsite/SignUp/Employer/Company/index.php
@@ -45,29 +45,56 @@ name, membership_id) VALUES (:user_id, :name, :membership_id);");
     }
 
 // 3.create payment method
-if(isset($_POST["membership_type"]) && isset($_POST["payment_method_type"])){
-print("<h2>Payment checker</h2>");
-}
+// if(isset($_POST["withdrawal_method"])){
+//   print($_POST["withdrawal_method"]);
+// }
+// if(isset($_POST["payment_method_type"])){
+//   print("<h2>Payment test 2</h2>");
+// } 
+// if(isset($_POST["billing_address"])){
+//   print("<h2>Payment test 3</h2>");
+// }
+// if(isset($_POST["postal_code"])){
+//   print("<h2>Payment test 4</h2>");
+// } 
+// if(isset($_POST["card_number"])){
+//   print("<h2>Payment test 5</h2>");
+// }
+// if(isset($_POST["security_code"])){
+//   print("<h2>Payment test 6</h2>");
+// } 
+// if(isset($_POST["expiration_month"])){
+//   print("<h2>Payment test 7</h2>");
+// }
+// if(isset($_POST["expiration_year"])){
+//   print("<h2>Payment test 8</h2>");
+// } 
     $payment = $conn->prepare("INSERT INTO ric55311.payment_methods (account_id, payment_method_type,
     billing_address, postal_code, card_number, security_code, expiration_month, expiration_year,
     withdrawal_method)
     VALUES (:account_id, :payment_method_type,
     :billing_address, :postal_code, :card_number, :security_code, :expiration_month, :expiration_year,
     :withdrawal_method);");
-    $account_id = $conn->prepare("SELECT id FROM ric55311.accounts WHERE user_id=:user_id;");
-    $account_id->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-    $payment->bindParam(':account_id', $account_id, PDO::PARAM_INT);
+    $query2 = $conn->prepare("SELECT id FROM ric55311.accounts WHERE user_id=:user_id;");
+    $query2->bindParam(":user_id",$user_id["id"]);
+    $query2->execute();
+    $account_id=$query2->fetch();
+    $payment->bindParam(':account_id', $account_id["id"], PDO::PARAM_INT);
     $payment->bindParam(':payment_method_type', $_POST["payment_method_type"]);
     $payment->bindParam(':billing_address', $_POST["billing_address"]);
     $payment->bindParam(':postal_code', $_POST["postal_code"]);
-    $payment->bindParam(':card_number', $_POST["card_number"]);
-    $payment->bindParam(':security_code', $_POST["security_code"]);
-    $payment->bindParam(':expiration_month', $_POST["expiration_month"]);
-    $payment->bindParam(':expiration_year', $_POST["expiration_year"]);
+    $card_number = intval($_POST["card_number"]);
+    $payment->bindParam(':card_number', $card_number, PDO::PARAM_INT);
+    $security_code = intval($_POST["security_code"]);
+    $payment->bindParam(':security_code', $security_code, PDO::PARAM_INT);
+    $expiration_month = intval($_POST["expiration_month"]);
+    $payment->bindParam(':expiration_month', $expiration_month, PDO::PARAM_INT);
+    $expiration_year = intval($_POST["expiration_year"]);
+    $payment->bindParam(':expiration_year', $expiration_year, PDO::PARAM_INT);
     $payment->bindParam(':withdrawal_method', $_POST["withdrawal_method"]);
 
     if($payment->execute()){
-        print("<h2>Your the mayment method for account " . $account_id . " was successfuly added</h2>");
+        print("<h2>Your the payment method for account " . $account_id["id"] . " was successfuly added</h2>");
     }
 
 
@@ -105,7 +132,7 @@ print("<h2>Payment checker</h2>");
       </div>
       <div class="form-group">
         <label for="phone">Phone Number</label><br>
-        <input type="text" class="form-control" name="phone" id="phone" required>
+        <input type="text" class="form-control" name="phone" id="phone" placeholder="123-123-1111"required>
       </div>
       <div class="form-group">
         <label for="password">Password (minimum 8 characters)</label><br>
@@ -135,19 +162,19 @@ print("<h2>Payment checker</h2>");
       </div>
       <div class="form-group">
         <label for="card_number">Card Number</label><br>
-        <input type="text" class="form-control" card_number="card_number" id="card_number" required>
+        <input type="text" class="form-control" name="card_number" id="card_number" placeholder="111122223333"required>
       </div>
       <div class="form-group">
         <label for="security_code">Security Code</label><br>
-        <input type="text" class="form-control" card_number="security_code" id="security_code" required>
+        <input type="text" class="form-control" name="security_code" id="security_code" placeholder="00111"required>
       </div>
       <div class="form-group">
         <label for="expiration_month">Expiration Month</label><br>
-        <input type="text" class="form-control" expiration_month="expiration_month" id="expiration_month" required>
+        <input type="text" class="form-control" name="expiration_month" id="expiration_month" placeholder="001"required>
       </div>
       <div class="form-group">
         <label for="expiration_year">Expiration Year</label><br>
-        <input type="text" class="form-control" expiration_month="expiration_year" id="expiration_year" required>
+        <input type="text" class="form-control" name="expiration_year" id="expiration_year" placeholder="2000"required>
       </div>
       <div class="form-group">
         <label for="billing_address">Billing Address</label><br>
