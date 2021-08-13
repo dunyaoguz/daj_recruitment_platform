4be62f9d68da0bdd0d137de4a0ee6398a1654d10
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2021-08-13 18:37:17 -0400

    fix relative path

--- JobPortalWebsite/SignUp/Employer/Company/index.php
+++ JobPortalWebsite/SignUp/Employer/Company/index.php
@@ -79,13 +79,13 @@ print("<h2>Payment checker</h2>");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../../style.css">
     <title>Employer Sign Up</title>
 </head>
 <body>
   <nav class="navbar navbar-light bg-light">
     <span class="navbar-brand mb-0 h1">DAJ Recruitment Platform</span>
-    <span class="logo-image"><img src="logo.png" class="logo"></span>
+    <span class="logo-image"><img src="../../../logo.png" class="logo"></span>
   </nav>
   <h2>Sign Up</h2>
   <h6>Fill the form below to sign up for a membership.</h6>
