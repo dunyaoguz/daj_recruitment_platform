55f8af0e8ce0bb6c804e4f2c74f96ee2ac260c6f
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2021-08-13 20:14:10 -0400

    login page updates

--- JobPortalWebsite/Login/Admin/index.php
+++ JobPortalWebsite/Login/Admin/index.php
@@ -13,18 +13,21 @@
     <span class="logo-image"><img src="../../logo.png" class="logo"></span>
   </nav>
   <h1>Administrator Login</h1>
-  <form action="" method="POST">
-    <input type="hidden" name="user_type" id="user_type" value="Administrator">
-    <div class="form-group">
-      <label for="login_name">Login Name:</label><br>
-      <input type="text" class="form-control" name="login_name" id="login_name" required>
-    </div>
-    <div class="form-group">
-      <label for="password">Password:</label><br>
-      <input type="password" class="form-control" name="password" id="password" minlength="8" required>
-    </div>
-    <p><button type="submit" class="btn btn-outline-success">Submit</button></p>
-  </form>
+  <div class="login">
+    <form action="" class="login-form" method="POST">
+      <input type="hidden" name="user_type" id="user_type" value="Administrator">
+      <div class="login-form-group">
+        <label for="login_name">Login Name:</label><br>
+        <input type="text" class="form-control" name="login_name" id="login_name" required>
+      </div>
+      <div class="login-form-group">
+        <label for="password">Password:</label><br>
+        <input type="password" class="form-control" name="password" id="password" minlength="8" required>
+      </div>
+      <a href='#' class="forgot-password">Forgot Password?</a>
+      <center><p><button type="submit" class="btn btn-outline-success">Log in</button></p></center>
+    </form>
+  </div>
   <div class="footer">
     © 2021 Copyright: Dunya Oguz, Azman Akhter, John Purcell
   </div>
