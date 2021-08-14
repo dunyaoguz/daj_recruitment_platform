5ba0b4032dd0d188beda49410aa7221712cb5b9f
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2021-08-13 21:48:53 -0400

    make login page the home page

--- JobPortalWebsite/index.php
+++ JobPortalWebsite/index.php
@@ -4,25 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>DAJ Recruitment Platform</title>
 </head>
 <body>
   <nav class="navbar navbar-light bg-light">
     <span class="navbar-brand mb-0 h1">DAJ Recruitment Platform</span>
-    <span class="logo-image"><img src="logo.png" class="logo"></span>
+    <span class="logo-image"><img src="../logo.png" class="logo"></span>
   </nav>
-  <p></p>
-  <h1>Welcome!</h1>
-  <p></p>
+  <h1>Login</h1>
+  <p><center>Select your user type:</center></p>
+
   <div class="d-flex justify-content-center">
     <div class="button">
-      <a href='./SignUp/' class="btn btn-outline-success btn-lg">Sign Up</a>
+      <a href='Login/Admin/' class="btn btn-outline-success btn-lg">Administrator</a>
     </div>
     <div class="button">
-      <a href='./Login/' class="btn btn-outline-success btn-lg">Login</a>
+      <a href='Login/Employer/' class="btn btn-outline-success btn-lg">Employer</a><br>
+    </div>
+    <div class="button">
+      <a href='Login/Recruiter/' class="btn btn-outline-success btn-lg">Recruiter</a><br>
+    </div>
+    <div class="button">
+      <a href='Login/JobSeeker/' class="btn btn-outline-success btn-lg">Job Seeker</a>
     </div>
   </div>
+  <br>
+  <a href='./SignUp/' class="register">Are you a new user? Register here</a>
   <div class="footer">
     © 2021 Copyright: Dunya Oguz, Azman Akhter, John Purcell
   </div>
