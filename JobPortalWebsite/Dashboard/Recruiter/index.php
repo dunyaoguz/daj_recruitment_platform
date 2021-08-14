9363a3f0852ddafc278d71db6412cb3710aff852
Azman Akhter <azman.akhter@gmail.com>
2021-08-14 18:38:26 -0400

    Updated Application Logic in Recruiter Dashboard

--- JobPortalWebsite/Dashboard/Recruiter/index.php
+++ JobPortalWebsite/Dashboard/Recruiter/index.php
@@ -17,13 +17,11 @@
     $recruiterEmployerId = $recruiterInfo['employer_id'];
 
     // Obtain Recruiter ID -> Obtain all Jobs for the Recruiter
-
     $getJobListingStmt = $conn->prepare("SELECT * FROM jobs WHERE recruiter_id = :recruiter_id");
     $getJobListingStmt->bindParam(':recruiter_id', $recruiterId, PDO::PARAM_INT);
     $getJobListingStmt->execute();
 
     // Obtain Membership ID of Employer -> fetch the Employer's membership Type
-
     $getEmployerMembershipIdStmt = $conn->prepare("SELECT * FROM employers WHERE user_id = :user_id");
     $getEmployerMembershipIdStmt->bindParam(':user_id', $recruiterEmployerId, PDO::PARAM_INT);
     $getEmployerMembershipIdStmt->execute();
@@ -35,22 +33,17 @@
     $getEmployerMembershipInfo = $getEmployerMembershipInfoStmt->fetch()['id'];
 
     // Get Total Number of Jobs For Employer
-
     $getTotalNumberOfJobsStmt = $conn->prepare("SELECT COUNT(id) FROM jobs WHERE employer_id = :employer_id");
     $getTotalNumberOfJobsStmt->bindParam(':employer_id', $recruiterEmployerId, PDO::PARAM_INT);
     $getTotalNumberOfJobsStmt->execute();
     $getTotalNumberOfJobs = $getTotalNumberOfJobsStmt->fetchColumn();
 
     // Check if the total jobs exceed or not
-
     if('1' == $getEmployerMembershipInfo && (int)$getTotalNumberOfJobs < 5){
-        echo "<h4> Inside 1st if block </h4> <br>"; 
         $canPostJob = TRUE;
-    }else if ('2' == $getEmployerMembershipInfo){
-        echo "<h4> Inside 2st if block </h4> <br>"; 
+    }else if ('2' == $getEmployerMembershipInfo){ 
         $canPostJob = TRUE;
     }else{
-        echo "<h4> Inside else block </h4> <br>"; 
         $canPostJob = FALSE;
     }
 
@@ -84,6 +77,7 @@
         </div>
       </nav>
       <h1><?php echo $recruiterName . "'s Dashboard";?></h1>
+      
       <h2>Your Jobs</h2>
       <h6>Here is a quick glance of all the jobs you've published with us.</h6>
       <br>
@@ -110,20 +104,69 @@
                   <td> <?php echo $row['required_experience']; ?> </td>
                   <td> <?php echo $row['status']; ?> </td>
                   <td>
-                    <a href="./edit.php?job_id=<?= $row["id"] ?>">Edit</a><br>    
-                    <a href="./delete.php?job_id=<?= $row["id"] ?>">Delete</a>
+                    <a href="./editJob.php?job_id=<?= $row["id"] ?>">Edit</a><br>    
+                    <a href="./deleteJob.php?job_id=<?= $row["id"] ?>">Delete</a>
                   </td>
                   </tr>
               <?php } ?>
           </tbody>
       </table>
+
       <br>
         <?php 
             if($canPostJob){ ?>
-                <center><a href="./create.php" class="btn btn-outline-success">Add a New Job</a></center>
+                <center><a href="./createJob.php" class="btn btn-outline-success">Add a New Job</a></center>
             <?php   } else {?>
-                <center><a href="./index.php" class="btn btn-outline-success">Limit Reached: Can't Post New Jobs</a></center>
+                <center><a href="./index.php" class="btn btn-outline-success">Limit Reached For Your Employer's Account: Can't Post New Jobs</a></center>
                 <?php   }?>
+      <br>
+
+      <h2>Your Applications</h2>
+      <h6>Here is a quick glance of all the applications.</h6>
+      <br>
+      <table class="table table-striped">
+          <thead>
+              <tr>
+                  <td>Application ID</td>
+                  <td>Applicant First Name</td>
+                  <td>Applicant Last Name</td>
+                  <td>Job ID</td>
+                  <td>Date_applied</td>
+                  <td>status</td>
+                  <td>Actions</td>
+              </tr>
+          </thead>
+
+          <tbody>
+             <?php while($jobList = $getJobListingStmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)){
+                    $jobId = $jobList['id'];
+
+                    $getApplicationListingStmt = $conn->prepare("SELECT * FROM applications WHERE job_id = :job_id");
+                    $getApplicationListingStmt->bindParam(':job_id', $jobId, PDO::PARAM_INT);
+                    $getApplicationListingStmt->execute();
+
+                    $getJobSeekerInfoStmt = $conn->prepare("SELECT * FROM job_seekers WHERE id = :id");
+                    $getJobSeekerInfoStmt->bindParam(':id', $getApplicationListingStmt->fetch()['job_seeker_id']);
+                    $getJobSeekerInfoStmt->execute();
+                    $getJobSeekerInfo =  $getJobSeekerInfoStmt->fetch();
+
+                    while ($row = $getJobListingStmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)){ ?>
+                        <tr>
+                        <td> <?php echo $row['id']; ?> </td>
+                        <td> <?php echo $getJobSeekerInfo['first_name']; ?> </td>
+                        <td> <?php echo $getJobSeekerInfo['last_name']; ?> </td>
+                        <td> <?php echo $row['job_id']; ?> </td>
+                        <td> <?php echo $row['date_applied']; ?> </td>
+                        <td> <?php echo $row['status']; ?> </td>
+                        <td>
+                            <a href="./edit.php?application_id=<?= $row["id"] ?>">Edit</a><br>    
+                        </td>
+                        </tr>
+
+                  <?php  }
+             } ?>
+          </tbody>
+      </table>
       <br>
       <div class="footer">
         © 2021 Copyright: Dunya Oguz, Azman Akhter, John Purcell
