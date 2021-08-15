1af7e67545be1aa813652cb95580b922a18f9b2a
Azman Akhter <azman.akhter@gmail.com>
2021-08-14 21:58:25 -0400

    All big fixes

--- JobPortalWebsite/Dashboard/Employer/delete.php
+++ JobPortalWebsite/Dashboard/Employer/delete.php
@@ -4,9 +4,9 @@ include_once('../../database.php');
 //$user_id = $_SESSION['userId'];
 //$user_id = "1";
 
-$recruiterDeleteStmt = $conn->prepare("DELETE FROM recruiters r
-                                            WHERE r.id = :r.id");
-$recruiterDeleteStmt->bindParam(':r.id', $_GET["recruiter_id"], PDO::PARAM_INT);
+$recruiterDeleteStmt = $conn->prepare("DELETE FROM recruiters
+                                            WHERE id = :id");
+$recruiterDeleteStmt->bindParam(':id', $_GET["recruiter_id"], PDO::PARAM_INT);
 
 if($recruiterDeleteStmt->execute()){
     header("Location: .");
