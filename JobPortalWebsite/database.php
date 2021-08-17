cba742d618f01a365871f5d84a469a32a4edbfa8
Dunya <dunya.oguz@mail.mcgill.ca>
2021-08-17 01:37:06 -0400

    Update database.php

--- JobPortalWebsite/database.php
+++ JobPortalWebsite/database.php
@@ -1,17 +1,12 @@
 <?php
 session_start();
-$server = 'ric5531.encs.concordia.ca:3306';
-$username ='ric55311';
-$password = 'YKWTFGO';
-$database = 'ric55311';
-// $server = '127.0.0.1';
-// $username ='root';
-// $password = 'dunya130995';
-// $database = 'recruitment_platform';
+$server = '127.0.0.1';
+$username ='root';
+$password = '...';
+$database = 'recruitment_platform';
 
 try{
     $conn = new PDO ("mysql:host=$server; port=3306; dbname=$database;", $username, $password);
-    // $conn = new PDO ("mysql:host=$server; dbname=$database;", $username, $password);
 }catch (PDOException $e){
     die('Connection Failed: ' . $e->getMessage());
 }
